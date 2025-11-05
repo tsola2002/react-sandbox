cae3b85a649505fe0e94904b7e41c6b7d65d4b61
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-05 11:28:55 +0100

    mui responsive grid

--- 07-UI-FRAMEWORKS/material-responsive-grid/src/Layout.jsx
+++ 07-UI-FRAMEWORKS/material-responsive-grid/src/Layout.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import {  Grid,  Paper, Typography } from "@mui/material";
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
 
 function Layout() {
   return (
@@ -15,6 +22,82 @@ function Layout() {
         }}
       >
         HEADER
+      </Paper>
+
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <Paper
+            sx={{
+              textAlign: "center",
+              fontWeight: "bold",
+              bgcolor: "yellow",
+            }}
+          >
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Nav Item 1" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Nav Item 2" />
+                </ListItemButton>
+              </ListItem>
+                        <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Nav Item 3" />
+                </ListItemButton>
+              </ListItem>
+                        <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Nav Item 4" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+          </Paper>
+        </Grid>
+
+        <Grid size={4}>
+          <Paper
+            sx={{
+              textAlign: "center",
+              fontWeight: "bold",
+              bgcolor: "red",
+            }}
+          >
+          Main Content 1
+          </Paper>
+        </Grid>
+
+        <Grid size={4}>
+          <Paper
+            sx={{
+              textAlign: "center",
+              fontWeight: "bold",
+              bgcolor: "pink",
+            }}
+          >
+          Main Content 2
+          </Paper>
+        </Grid>
+
+      
+      </Grid>
+
+      <Paper
+        sx={{
+          p: 2,
+          textAlign: "center",
+          fontWeight: "bold",
+          bgcolor: "orange",
+          width: "100vw",
+          ml: -2, // cancel out the parent padding
+        }}
+      >
+        FOOTER
       </Paper>
     </Box>
   )
