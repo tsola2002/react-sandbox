6eb90522daad3ef0630838d98e1d589c48ef4f43
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-17 12:22:18 +0100

    login conditional

--- 08-BACKEND-AS-A-SERVICE/blog-app/src/Navbar.jsx
+++ 08-BACKEND-AS-A-SERVICE/blog-app/src/Navbar.jsx
@@ -18,12 +18,20 @@ function Navbar() {
     <div className="navbar">
         <div className="links">
           <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
+          {!user ? (
+          <Link to="/login"> Login </Link>
+          ) : (
+            <Link to="/createpost"> Create Post </Link>
+          )}
         </div>
         <div className="user">
-          <p>{auth.currentUser?.displayName}</p>
-          <img src={auth.currentUser?.photoUrl || ""} width="100" height="100" />
-          <button onClick={signUserOut}>Log Out</button>
+          {user && (
+          <>
+            <p> {user?.displayName} </p>
+            <img src={user?.photoURL || ""} width="20" height="20" />
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+          )}
         </div>
     </div>
   );
